dfb6aea7a72fe60a5c0a00fd83664a9cde4f48b0
tdh <tdh@tdh.com>
2021-06-24 09:56:12 +0800

    main

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //这里是v1.0.0版本
+        1.asdjakfda
+            2.asdfasdfajkkadf
+            3.sdfasdfa
+            4.efasfa
     }
 }
